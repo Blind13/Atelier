c4bdb6f7d3ea9c0de11d417219544a584fe34fd2
unknown <oma.hai@yahoo.com>
2021-02-01 17:44:00 +0100

    TP

---
+++ main.js
@@ -0,0 +1,60 @@
+var myDiv = document.querySelector("div");
+var myRadio = document.getElementsByClassName("radio");
+var nom = document.getElementById("nom");
+var oui = document.getElementById("oui");
+var no = document.getElementById("no");
+var nom = document.getElementById("nom");
+var message = document.getElementById("message");
+var slect = document.querySelector("select");
+var sub = document.querySelector("#sub");
+
+//affiche and remove
+function affiche() {
+  myRadio[0].onclick = function () {
+    if (oui.checked) {
+      myDiv.classList.remove('hidden');
+    }
+  };
+  myRadio[1].onclick = function () {
+    if (no.checked)
+      myDiv.classList.add('hidden');
+  };
+  return;
+}
+affiche();
+
+var exp = new RegExp('^[A-Za-z]+$');
+var checkExp = false;
+nom.onkeyup = () =>{
+  if(exp.test(nom.velue) == true){
+    message.innerHTML = "le nom validé";
+    checkExp = true;
+  } else {
+    message.innerHTML = "le nom n'est pas validé";
+    checkExp = false;
+  }
+}
+
+var chekingselect=false;
+
+slect.onclick = function (){
+    if(slect.selectedIndex !=0)
+    {
+        console.log("thiscis regexp 1 "+ exp.test(nom.value) );
+        chekingselect = true;
+    }
+    if(slect.selectedIndex == 0)
+    {
+        
+        console.log("this is regexp 2 "+ exp.test(nom.value) );
+        chekingselect=false;
+    }
+}
+
+sub.addEventListener("click",function(e){
+  if(chekingselect==false || checkRgexp==false)
+   {
+      e.preventDefault();
+   }
+});
+
